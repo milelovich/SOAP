62a873cbbfd087a782eeae1d97c25d9e0dbec323
milelovich <milelovich@mail.ru>
2022-01-24 20:41:02 +0400

    HomeWork-4

---
+++ src/main/java/com/geekbrains/spring/web/endpoints/ProductEndpoint.java
@@ -0,0 +1,44 @@
+package com.geekbrains.spring.web.endpoints;
+
+import com.flamexander.spring.ws.services.ProductService;
+import com.flamexander.spring.ws.soap.products.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+@RequiredArgsConstructor
+public class ProductEndpoint<ProductService> {
+    private static final String NAMESPACE_URI = "http://www.flamexander.com/spring/ws/products";
+    private final ProductService productService;
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getProductByNameRequest")
+    @ResponsePayload
+    public GetProductByNameResponse getProductByName(@RequestPayload GetProductByNameRequest request) {
+        GetProductByNameResponse response = new GetProductByNameResponse();
+        response.setProduct(productService.getByName(request.getName()));
+        return response;
+    }
+
+    /*
+        Пример запроса: POST http://localhost:8080/ws
+        Header -> Content-Type: text/xml
+
+        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:f="http://www.flamexander.com/spring/ws/products">
+            <soapenv:Header/>
+            <soapenv:Body>
+                <f:getAllProductsRequest/>
+            </soapenv:Body>
+        </soapenv:Envelope>
+     */
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getAllProductsRequest")
+    @ResponsePayload
+    public GetAllProductsResponse getAllProducts(@RequestPayload GetAllProductsRequest request) {
+        GetAllProductsResponse response = new GetAllProductsResponse();
+        productService.getAllProducts().forEach(response.getProducts()::add);
+        return response;
+    }
+}
